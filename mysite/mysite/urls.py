62fffd47c916ab0ed5b26a260690089dfea75ead
MyBhive <delphine.miel0318@gmail.com>
2021-05-14 20:20:48 +0200

    'try'

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+<<<<<<< HEAD
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,4 +22,14 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
+=======
+
+from django.urls import path, include
+from purbeurre.views import home, search_product
+
+
+urlpatterns = [
+    path('', home, name='pages/home'),
+    path('', include('purbeurre.urls'))
+>>>>>>> mise a jour
 ]
