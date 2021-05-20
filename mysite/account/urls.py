a6815392db39b7003f90ea70e0e294f0b82d0bca
MyBhive <delphine.miel0318@gmail.com>
2021-05-21 00:00:31 +0300

    link views to frontend

---
+++ mysite/account/urls.py
@@ -0,0 +1,17 @@
+﻿# coding: utf-8
+from django.urls import path
+
+from . import views
+
+"""Path to connect views to the frontend"""
+
+urlpatterns = [
+    path('login/', views.log_in, name="login"),
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('log_out/', views.log_out, name='log_out'),
+    path('mon_compte/', views.my_account, name='my_account'),
+    path('mes favoris/<str:product_id>/',
+         views.save_in_favorite,
+         name='save_in_favorite'),
+    path('mes favoris', views.my_favorites_view, name="my_favorites_view")
+]
