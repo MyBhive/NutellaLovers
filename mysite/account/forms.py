8c23233e8a26e9830ca31d9077bd408617de7192
MyBhive <delphine.miel0318@gmail.com>
2021-05-20 23:59:58 +0300

    creating django forms: user sign_in

---
+++ mysite/account/forms.py
@@ -0,0 +1,13 @@
+﻿from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignInForm(UserCreationForm):
+    """Create form for sign_in"""
+    username = forms.CharField()
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
