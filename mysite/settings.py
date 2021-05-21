a10f1ed6b7f418763c1cfc9c02012ca6e9e2419f
MyBhive <delphine.miel0318@gmail.com>
2021-05-21 23:03:19 +0300

    db'

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,7 +57,6 @@ else:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES = {}
     DATABASES['default'] = db_from_env
 
     ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nutellalovers.herokuapp.com"]
