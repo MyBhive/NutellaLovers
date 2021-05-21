12bf5f3e1e2e10f0569bf3d3d1f992ea30bed0cb
MyBhive <delphine.miel0318@gmail.com>
2021-05-21 18:43:02 +0300

    added database conn

--- mysite/settings.py
+++ mysite/settings.py
@@ -94,8 +94,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES = {}
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'] = db_from_env
 
 # Password validation
