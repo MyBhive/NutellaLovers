02db767608e294c4fc10f800f39ce0595a4a07a5
MyBhive <delphine.miel0318@gmail.com>
2021-05-21 16:30:25 +0300

    tree

--- mysite/settings.py
+++ mysite/settings.py
@@ -142,6 +142,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
