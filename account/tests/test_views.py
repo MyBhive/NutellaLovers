b97e280c95393a442c365e4891b2e766af3fd617
MyBhive <delphine.miel0318@gmail.com>
2021-06-07 00:34:43 +0300

    integrate 'assertEqual' by selenium

--- account/tests/test_views.py
+++ account/tests/test_views.py
@@ -151,50 +151,54 @@ class SaveAndViewTestCaseOfFavorite(TestCase):
         self.assertEquals(response.status_code, 302)
 
 
-class TestForm(LiveServerTestCase):
+class TestSeleniumUserPath(LiveServerTestCase):
+    def setUp(self):
+        self.category = CategoryProduct.objects.create(
+            name_category="pates-a-tartiner"
+        )
+        self.substitute = ProductInfo.objects.create(
+            name_product="Duo",
+            category=self.category,
+            nutrition_grade="b",
+            image_product="www.image-duo.com",
+            url_product="www.duo.com",
+            image_nutrition="www.img_nutri.com"
+        )
+
+        self.prod_info = ProductInfo.objects.create(
+            name_product="nutella",
+            category=self.category,
+            nutrition_grade="e",
+            image_product="www.image-nutellat.com",
+            url_product="www.nutella.com",
+            image_nutrition="www.img_nutri.com"
+        )
 
     def test_form_sign_in(self):
+        # sign_in
         driver = webdriver.Chrome('account/tests/chromedriver.exe')
         driver.implicitly_wait(30)
         driver.get('%s%s' % (self.live_server_url, '/sign_in/'))
-        username = driver.find_element_by_name('username')
-        email = driver.find_element_by_name('email')
-        password1 = driver.find_element_by_name('password1')
-        password2 = driver.find_element_by_name('password2')
-        submit = driver.find_element_by_id('valid_sign_in')
-        # fill in the form with data
-        username.send_keys('selenium')
-        email.send_keys('selenium@gmail.com')
-        password1.send_keys('champignon1')
-        password2.send_keys('champignon1')
-        # submit form
-        submit.click()
-        """test de redirection"""
-        time.sleep(1)
-        driver.quit()
-
-    def test_form_log_in(self):
-        driver = webdriver.Chrome('account/tests/chromedriver.exe')
-        driver.implicitly_wait(30)
-        driver.get('%s%s' % (self.live_server_url, '/login/'))
-        username = driver.find_element_by_name('username')
-        password = driver.find_element_by_name('password')
-
-        username.send_keys('selenium@gmail.com')
-        password.send_keys('champignon1')
-        submit = driver.find_element_by_id('valid_log_in')
-        submit.click()
-        """test de redirection"""
-        time.sleep(1)
-        driver.quit()
-
-    def test_research_product(self):
-        driver = webdriver.Chrome('account/tests/chromedriver.exe')
-        driver.implicitly_wait(30)
+        self.assertEqual(len(User.objects.all()), 0)
+        driver.find_element_by_name('username').send_keys('selenium')
+        driver.find_element_by_name('email').send_keys('selenium@gmail.com')
+        driver.find_element_by_name('password1').send_keys('champignon1')
+        driver.find_element_by_name('password2').send_keys('champignon1')
+        driver.find_element_by_id('valid_sign_in').click()
+        self.assertEqual(driver.title, 'Log in')
+        self.assertEqual(len(User.objects.all()), 1)
+        # log in
+        driver.find_element_by_name('username').send_keys('selenium')
+        driver.find_element_by_name('password').send_keys('champignon1')
+        driver.find_element_by_id('valid_log_in').click()
+        user_log = User.objects.get(username='selenium')
+        self.assertTrue(user_log.check_password('champignon1'))
         driver.get('%s%s' % (self.live_server_url, '/'))
-        research = driver.find_element_by_name('user_research')
-        research.send_keys('potiron')
-        submit = driver.find_element_by_name('research')
-        submit.click()
-        time.sleep(1)
+        self.assertEqual(driver.title, 'Pur Beurre')
+        # search product
+        driver.find_element_by_name('user_research').send_keys('nutella')
+        driver.find_element_by_name('research').click()
+        self.assertEqual(driver.title, 'Recherche')
+        driver.find_element_by_name('save').click()
+        time.sleep(5)
         driver.quit()
