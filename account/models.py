448e3dcf8e15e3baae6a8b614f2b6c73a417727d
MyBhive <delphine.miel0318@gmail.com>
2021-08-01 17:44:36 +0200

    P11 refacto: add class CostumUser(Abstractuser)

--- account/models.py
+++ account/models.py
@@ -1,2 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CostumUser(AbstractUser):
+    """This model override the 'django user model' that we can work on it"""
+    def __str__(self):
+        return self.username
