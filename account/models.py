454a710d83ece3526f094153b1c0eec7521d1cf9
MyBhive <delphine.miel0318@gmail.com>
2021-08-11 14:08:54 +0300

    add date of birth in AbstractUser for refacto

--- account/models.py
+++ account/models.py
@@ -4,5 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     """This model override the 'django user model' that we can work on it"""
+    date_of_birth = models.DateField(default=None, null=True)
+
     def __str__(self):
         return self.username
